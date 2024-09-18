cb70537177a9c38ca354f39d7c69d0c5a6451326
AkankshaSinghK <singhakanksha2897@gmail.com>
2024-09-18 21:38:25 +0530

    third commit

--- src/components/Step3.js
+++ src/components/Step3.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 function Step3({ formData, prevStep, submitForm }) {
   const handleSubmit = () => {
@@ -10,46 +10,72 @@ function Step3({ formData, prevStep, submitForm }) {
 
   return (
     <div className="container mt-4">
-      <h3 className="mb-4 p-3 text-center card-header text-white">Confirm Your Information</h3>
-      <table className="table table-bordered table-striped">
-        <tbody>
-          <tr>
-            <td><strong>Name</strong></td>
-            <td>{formData.name}</td>
-          </tr>
-          <tr>
-            <td><strong>Email</strong></td>
-            <td>{formData.email}</td>
-          </tr>
-          <tr>
-            <td><strong>Phone</strong></td>
-            <td>{formData.phone}</td>
-          </tr>
-          <tr>
-            <td><strong>Address Line 1</strong></td>
-            <td>{formData.address1}</td>
-          </tr>
-          <tr>
-            <td><strong>Address Line 2</strong></td>
-            <td>{formData.address2}</td>
-          </tr>
-          <tr>
-            <td><strong>City</strong></td>
-            <td>{formData.city}</td>
-          </tr>
-          <tr>
-            <td><strong>State</strong></td>
-            <td>{formData.state}</td>
-          </tr>
-          <tr>
-            <td><strong>Zip Code</strong></td>
-            <td>{formData.zip}</td>
-          </tr>
-        </tbody>
-      </table>
+      <h3 className="mb-4 p-3 text-center card-header text-white">
+        Confirm Your Information
+      </h3>
+      <div className="table-responsive">
+        <table className="table table-bordered table-striped">
+          <tbody>
+            <tr>
+              <td>
+                <strong>Name</strong>
+              </td>
+              <td>{formData.name}</td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Email</strong>
+              </td>
+              <td>{formData.email}</td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Phone</strong>
+              </td>
+              <td>{formData.phone}</td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Address Line 1</strong>
+              </td>
+              <td>{formData.address1}</td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Address Line 2</strong>
+              </td>
+              <td>{formData.address2}</td>
+            </tr>
+            <tr>
+              <td>
+                <strong>City</strong>
+              </td>
+              <td>{formData.city}</td>
+            </tr>
+            <tr>
+              <td>
+                <strong>State</strong>
+              </td>
+              <td>{formData.state}</td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Zip Code</strong>
+              </td>
+              <td>{formData.zip}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
       <div className="d-flex justify-content-between mt-4 ">
-        <button className="btn btn-secondary w-50 me-5" onClick={prevStep}> <i className="bi bi-arrow-left-circle me-3"></i>Back</button>
-        <button className="btn btn-success w-50" onClick={handleSubmit}>Submit</button>
+        <button className="btn btn-secondary w-50 me-5" onClick={prevStep}>
+          {" "}
+          <i className="bi bi-arrow-left-circle me-3"></i>Back
+        </button>
+        <button className="btn btn-success w-50" onClick={handleSubmit}>
+          Submit
+        </button>
       </div>
     </div>
   );
